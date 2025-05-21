21ef04dc054673ecbe7097b7f99b3f4b00e03880
Hoan Phan <58675202+pnHoan@users.noreply.github.com>
2025-05-21 17:52:09 +0700

    fix update mirror

--- 901-update_mirror.sh
+++ 901-update_mirror.sh
@@ -8,7 +8,7 @@ echo "########################################################################"
 tput sgr0
 echo
 
-$pkg="reflector"
+pkg="reflector"
 
 if pacman -Qi "$pkg" &>/dev/null; then
     sudo reflector --age 6 --latest 20  --fastest 20 --threads 20 --sort rate --protocol https --save /etc/pacman.d/mirrorlist
